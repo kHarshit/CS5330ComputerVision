d0c5fbfe8a145444e2d7d4ebecadf5239f797d77
Harshit Kumar <kumar.hars@northeastern.edu>
2024-01-25 18:12:47 -0500

    Add more effects

--- project1_video_special_effects/src/vidDisplay.cpp
+++ project1_video_special_effects/src/vidDisplay.cpp
@@ -31,6 +31,9 @@ int displayVideo(int videoDeviceIndex) {
     cv::Mat grey;
     std::vector<cv::Rect> faces;
     cv::Rect last(0, 0, 0, 0);
+    cv::VideoWriter video;
+    bool isVideoWriterInitialized = false;
+    bool isSavingVideo = false;
 
     char lastKeypress = '\0';  // Initialize the last keypress variable
 
@@ -41,31 +44,44 @@ int displayVideo(int videoDeviceIndex) {
             break;
         }
 
+        if (!isVideoWriterInitialized) {
+            video.open("out.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'), 10, cv::Size(frame.cols, frame.rows));
+            isVideoWriterInitialized = true;
+        }
+
         // Check the last keypress and modify the image accordingly
         if (lastKeypress == 'g') {
+            cv::putText(frame, "OpenCV Greyscale", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
         } else if (lastKeypress == 'h') {
             // Use the custom greyscale function
+            cv::putText(frame, "Custom Greyscale", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             greyscale(frame, frame);
         } else if (lastKeypress == 't') {
             // Use the custom sepia function
+            cv::putText(frame, "Custom Sepia", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             sepia(frame, frame);
         } else if (lastKeypress == 'b') {
             // Use the custom blur function
+            cv::putText(frame, "Custom Blur", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             blur5x5_1(frame, frame);
         } else if (lastKeypress == 'B') {
             // Use the custom (faster) blur function
+            cv::putText(frame, "Custom Blur (faster)", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             blur5x5_2(frame, filter);
             cv::convertScaleAbs(filter, frame);
         } else if (lastKeypress == 'x') {
+            cv::putText(frame, "SobelX", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             sobelX3x3(frame, filter);
             cv::convertScaleAbs(filter, frame);
             // cv::imshow("SobelX", filter);
         } else if (lastKeypress == 'y') {
+            cv::putText(frame, "SobelY", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             sobelY3x3(frame, filter);
             cv::convertScaleAbs(filter, frame);
             // cv::imshow("SobelY", filter);
         } else if (lastKeypress == 'f') {
+            cv::putText(frame, "Face Detect", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             // use facedetect
             // convert the image to greyscale
             cv::cvtColor( frame, grey, cv::COLOR_BGR2GRAY, 0);
@@ -81,6 +97,7 @@ int displayVideo(int videoDeviceIndex) {
             last.height = (faces[0].height + last.height)/2;
             }
         } else if (lastKeypress == 'c') {
+            cv::putText(frame, "Colorful Face", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             // Make the face colorful, while the rest of the image is greyscale.
             // use facedetect
             // convert the image to greyscale
@@ -110,9 +127,11 @@ int displayVideo(int videoDeviceIndex) {
             cv::cvtColor(mask, mask3, cv::COLOR_GRAY2BGR);
             frame = (frame & mask3) + (gray & ~mask3);
         } else if (lastKeypress == 'n') {
+            cv::putText(frame, "Negative Image", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 0, 255), 3);
             // negative image
             frame = 255 - frame;
         } else if (lastKeypress == 'e') {
+            cv::putText(frame, "Embossing Effect", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(0, 255, 0), 3);
             // Make an embossing effect
             cv::Mat grad_x, grad_y;
             sobelX3x3(frame, grad_x);
@@ -120,6 +139,21 @@ int displayVideo(int videoDeviceIndex) {
             cv::convertScaleAbs(grad_x, grad_x);
             cv::convertScaleAbs(grad_y, grad_y);
             frame = grad_x * 0.7071 + grad_y * 0.7071; 
+        } else if (lastKeypress == 'v') {
+            cv::putText(frame, "Video Saving Started", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(255, 0, 0), 3);
+            // save short video sequences with the special effects
+            // save the current frame to the video
+            isSavingVideo = true;
+        } else if (lastKeypress == '0') {
+            cv::putText(frame, "Video Saving Stopped", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(255, 0, 0), 3);
+            // stop video saving
+            isSavingVideo = false;
+        } else {
+            cv::putText(frame, "Original Video", cv::Point(30, 50), cv::FONT_HERSHEY_DUPLEX, 1.5, cv::Scalar(255, 0, 0), 3);
+        }
+
+        if (isSavingVideo) {
+            video.write(frame);
         }
 
         cv::imshow("Video", frame);
@@ -170,6 +204,12 @@ int displayVideo(int videoDeviceIndex) {
         } else if (key == 'e') {
             lastKeypress = 'e';
             cout << lastKeypress << "pressed : Make embossing effect." << endl;
+        } else if (key == 'v') {
+            lastKeypress = 'v';
+            cout << lastKeypress << "pressed : Video saving started." << endl;
+        } else if (key == '0') {
+            lastKeypress = '0';
+            cout << lastKeypress << "pressed : Video saving stopped." << endl;
         }
     }
 
