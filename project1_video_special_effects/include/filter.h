749a5d957793f804fda8d6b40d1c16edf92f29ff
Khushi-12 <73729055+Khushi-12@users.noreply.github.com>
2024-01-26 06:41:28 -0500

    Update filter.h

--- project1_video_special_effects/include/filter.h
+++ project1_video_special_effects/include/filter.h
@@ -1,8 +1,8 @@
 /**
- * author: Harshit Kumar
+ * author: Harshit Kumar and Khushi Neema
  * date: Jan 20, 2024
  * purpose: Applies a filter to an image.
- * 
+ *
 */
 
 #ifndef FILTER_H
@@ -15,7 +15,7 @@
  * @param src Input image.
  * @param dst Output image.
  * @return 0 if the operation is successful.
- * 
+ *
 */
 int greyscale(cv::Mat& src, cv::Mat& dst);
 
@@ -24,7 +24,7 @@ int greyscale(cv::Mat& src, cv::Mat& dst);
  * @param src Input image.
  * @param dst Output image.
  * @return 0 if the operation is successful.
- * 
+ *
 */
 int sepia(cv::Mat& src, cv::Mat& dst);
 
@@ -34,7 +34,7 @@ int sepia(cv::Mat& src, cv::Mat& dst);
  * @param dst Output image.
  * @return 0 if the operation is successful.
 */
-int blur5x5_1( cv::Mat &src, cv::Mat &dst );
+int blur5x5_1(cv::Mat& src, cv::Mat& dst);
 
 /**
  * @brief Applies a custom blur filter to an image (faster version).
@@ -42,7 +42,7 @@ int blur5x5_1( cv::Mat &src, cv::Mat &dst );
  * @param dst Output image.
  * @return 0 if the operation is successful.
 */
-int blur5x5_2( cv::Mat &src, cv::Mat &dst );
+int blur5x5_2(cv::Mat& src, cv::Mat& dst);
 /*
 * @brief Applies SobelX filter (horizontal edge detection) to an image
 * @param src Input image
@@ -57,8 +57,15 @@ int sobelX3x3(cv::Mat& src, cv::Mat& dst);
 * @return 0 if the operation is succesful.
 */
 int sobelY3x3(cv::Mat& src, cv::Mat& dst);
-
-/** 
+/**
+ * @brief generates a gradient magnitude to an image.
+ * @param sx SobelX values.
+ * @param sy SobelY values.
+ * @param dst Output image.
+ * @return 0 if the operation is successful.
+*/
+int magnitude(cv::Mat& sx, cv::Mat& sy, cv::Mat& dst);
+/**
  * @brief Applies a custom comic book effect to an image.
  * @param src Input image.
  * @param dst Output image.
