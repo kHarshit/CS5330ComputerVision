971a6dfd57d50eb31d9b330e9470a5337d9a5f52
Harshit Kumar <kumar.hars@northeastern.edu>
2024-02-08 23:23:38 -0500

    Clean code

--- project2_content_based_image_retrieval/main.cpp
+++ project2_content_based_image_retrieval/main.cpp
@@ -23,18 +23,16 @@ using namespace std;
 // if RUN_PART1 is 0 and you want to write data to csv, set it to 1
 #define WRITE_CSV 0
 
-void Sort(std::vector<std::pair<std::string, double> >& matches) {
-    int n = matches.size();
-    for (int i = 0; i < n - 1; ++i) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; ++j) {
-            if (matches[j].second < matches[minIndex].second) {
-                minIndex = j;
-            }
-        }
-        if (minIndex != i) {
-            std::swap(matches[i], matches[minIndex]);
-        }
+void sort(std::vector<std::pair<std::string, double> > &distances, bool ascending = true) {
+    // Sorting the distances in ascending or descending order based on the 'ascending' argument
+    if (ascending) {
+        std::sort(distances.begin(), distances.end(), [](const std::pair<std::string, double> &a, const std::pair<std::string, double> &b) {
+            return a.second < b.second;
+        });
+    } else {
+        std::sort(distances.begin(), distances.end(), [](const std::pair<std::string, double> &a, const std::pair<std::string, double> &b) {
+            return a.second > b.second;
+        });
     }
 }
 
@@ -51,8 +49,6 @@ int main(int argc,char *argv[])
 
     DIR *dirp;
     struct dirent *dp;
-    std::cout<<"Argument 1";
-    printf(argv[1]);
     dirp = opendir(argv[1]);
     if (dirp == NULL) {
         std::cout << "Cannot open directory " << argv[1] << std::endl;
@@ -81,8 +77,6 @@ int main(int argc,char *argv[])
         std::cout << "Usage: " << argv[0] << " <target image> <feature vector file> <N>" << std::endl;
         return -1;
     }
-    printf("Argument 1");
-    printf(argv[1]);
     cv::Mat target_image = cv::imread(argv[1]);
     if (target_image.empty()) {
         std::cout << "Could not read the target image." << std::endl;
@@ -113,139 +107,11 @@ int main(int argc,char *argv[])
     }
 
     // Sorting the distances in ascending order
-    std::sort(distances.begin(), distances.end(), [](const std::pair<std::string, double> &a, const std::pair<std::string, double> &b) {
-        return a.second < b.second;
-    });
+    sort(distances);
 
     // Displaying top N matches, starting from the second match to avoid the target image itself if present
     for (int i = 1; i <= N && i < distances.size(); ++i) {
         std::cout << "Distance: " << distances[i].second << ", File: " << distances[i].first << std::endl;
-    }
-
-
-    #endif
-
-    #if 0
-    char dirname[256];
-    char buffer[256];
-    int flag=0;
-    FILE *fp;
-    DIR *dirp;
-    struct dirent *dp;
-    int i;
-    std::vector<std::pair<std::string, double> > matches; 
-    char filename[]="Feature_Vector.csv";
-    cv::Mat target_image,target_features;
-    std::vector<float> featureVector;
-    if (argc < 2)
-    {
-        printf("usage: %s <directory path>\n", argv[0]);
-        exit(-1);
-    }
-    strcpy(dirname, argv[1]);
-    printf("Processing directory %s\n", dirname);
-
-
-    //Reading the target image and computing its features
-    target_image=cv::imread("/home/kharshit/Khushi/olympus/pic.0503.jpg");
-    //cv::imshow("Target Image",target_image);
-    //cv::waitKey(0);
-    cout << "Computing target image features\n";
-    target_features=computeBaselineFeatures(target_image);
-
-    dirp = opendir(dirname);
-    if (dirp == NULL)
-    {
-        printf("Cannot open directory %s\n", dirname);
-        exit(-1);
-    }
-    #if WRITE_CSV
-    cout << "Writing to CSV file\n";
-    while ((dp = readdir(dirp)) != NULL)
-    {
-
-        // check if the file is an image
-        if (strstr(dp->d_name, ".jpg") ||
-            strstr(dp->d_name, ".png") ||
-            strstr(dp->d_name, ".ppm") ||
-            strstr(dp->d_name, ".tif"))
-        {
-            strcpy(buffer, dirname);
-            strcat(buffer, "/");
-            strcat(buffer, dp->d_name);
-            //printf("full path name %s",buffer);
-        
-        cv::Mat current_image=cv::imread(buffer);
-        cv::Mat current_features=computeBaselineFeatures(current_image);
-        
-        //Convert cv::Mat to std::vector<float>
-        
-        featureVector.assign(current_features.begin<float>(), current_features.end<float>());
-         if(flag==0){
-             flag=1;
-            int status=append_image_data_csv(filename,dp->d_name,featureVector,1);
-        }
-        std::vector<float> featureVector;
-        current_features.reshape(1, 1).convertTo(featureVector, CV_32F);
-        int status=append_image_data_csv(filename,dp->d_name,featureVector,0);
-        if(status!=0){
-            printf("Error in writing information in file");
-        }
-        }
-             
-    }
-    #endif
-    cout << "Reading from CSV file\n";
-    std::vector<char*> filenames;
-    std::vector<std::vector<float> >  data;
-    if (read_image_data_csv(filename, filenames, data, 0) != 0) {
-        printf("Error: Unable to read image data from CSV file.\n");
-        return -1;
-    }
-//     for (int i = 0; i < data.size(); ++i) {
-//     std::cout << "Filename: " << filenames[i] << std::endl;
-//     std::cout << "Feature Vector:";
-//     cv::Mat featureMat(256, 320, CV_32F);
-//     memcpy(featureMat.data, data[i].data(), data[i].size() * sizeof(float));
-//     //featureMat=featureMat.reshape(1,1);
-//     std::cout << featureMat.rows <<" ";
-//     std::cout<<featureMat.cols;
-//     std::cout << std::endl;
-//     break;
-// }
-    
-    
-    cout << "Computing distances\n";
-    //Compute distances between target image features and features of all other images
-    for (int i = 0; i < filenames.size(); ++i) {
-        cout << filenames[i] << endl;
-        
-        if (strcmp(filenames[i], "pic.0503.jpg") != 0) {  // Exclude the target image itself
-            // cv::Mat featureMat(target_features.rows,target_features.cols, CV_32F);
-            // memcpy(featureMat.data, data[i].data(), data[i].size() * sizeof(float));
-            // std::cout << " Target Size " << target_features.rows << " "<<target_features.cols;
-            //std::cout << " Input Size " << featureMat.rows << " "<<featureMat.cols;
-            //std::cout << "Type of Input " << featureMat.type();
-            std::cout<< "Type of Target " << target_features.type();
-            
-            double distance = computeDistance(target_features, cv::Mat(data[i]).reshape(256,320));
-            matches.push_back(std::make_pair(std::string(filenames[i]), distance));
-        }
-    }
-
-    // Sort matches based on distance
-    Sort(matches);
-
-    // Print top N matches
-    int N = 5;  // Number of closest matches to print
-    std::cout << "Top " << N << " closest matches:" << std::endl;
-    for (int i = 0; i < N && i < matches.size(); ++i) {
-        std::cout << "Filename: " << matches[i].first << ", Distance: " << matches[i].second << std::endl;
-    }
-
-    // Clean up memory
-    for (int i = 0; i < filenames.size(); ++i) {
-        delete[] filenames[i];
     }
 
     #endif
@@ -326,10 +192,7 @@ int main(int argc, char *argv[])
     }
     closedir(dirp);
 
-    // Sorting the distances in ascending order
-    std::sort(distances.begin(), distances.end(), [](const std::pair<std::string, double> &a, const std::pair<std::string, double> &b) {
-        return a.second > b.second;
-    });
+    sort(distances, false);
 
     int N = 3; // Number of top matches to display
     printf("Top %d matches:\n", N);
