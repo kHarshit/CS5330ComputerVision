288639a505009f37831098109dd47786bfcb1b47
Khushi-12 <nhushikeema12@gmaill.com>
2024-02-19 16:02:13 -0500

    HSV conversion

--- project3_object_recognition/main.cpp
+++ project3_object_recognition/main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 using namespace cv;
@@ -66,29 +67,56 @@ int main()
 {
     //to open a default camera
     cv::VideoCapture cap(0); 
-    cv::Mat frame,blur;
+    cv::Mat frame,blur,hsv;
     if(!cap.isOpened())
     {
         printf("Error opening the default camera");
         return -1;
     }
     cv::namedWindow("Original Video",WINDOW_NORMAL);
-    cv::namedWindow("Blurred Video",WINDOW_NORMAL);
+    cv::namedWindow("Thresholded",WINDOW_NORMAL);
     for(;;)
     {
-        cap>>frame;
         
-        cv::imshow("Original Video",frame);
-        cv::cvtColor(frame,frame,cv::COLOR_BGR2GRAY,0);
+        #if 0
+        //cv::Mat frame;
+        cap>>frame;
+        if (frame.empty()) {
+            std::cout << "Error: Blank frame grabbed" << std::endl;
+            continue;
+        }
+        //cv::imshow("Original Video",frame);
+        //cv::imshow("Blurred Video",hsv);
+        
         blur5x5_2(frame,blur);
-        cv::imshow("Blurred Video",blur);
+        
+        //cv::cvtColor(frame,frame,cv::COLOR_BGR2GRAY);
         cv::convertScaleAbs(blur, blur);
-        cv::cvtColor()
+    
+        Mat hsv;
+        cvtColor(frame, hsv, COLOR_BGR2HSV);
+
+        // // Dynamically set the threshold using ISODATA algorithm
+        std::vector<Mat> channels;
+        split(hsv, channels);
+        Mat sat = channels[1]; // Saturation channel
+        double minVal, maxVal;
+        minMaxLoc(sat, &minVal, &maxVal);
+        double thresholdValue = (minVal + maxVal) / 2.0;
+
+        // // Thresholding
+        Mat thresholded;
+        cv::threshold(sat, thresholded, thresholdValue, 255, THRESH_BINARY);
+
+        // Display original and thresholded video
+        cv::imshow("Original Video", frame);
+        cv::imshow("Thresholded", thresholded);
+
         char key = cv::waitKey(10);
         if (key == 'q') {
             break;
         }
-
+        #endif
     }
 
     cap.release();
